9356a86b0826c3257a4205d543ecff106f9936d6
lbulegon <lbulegon@gmail.com>
2025-06-10 01:42:09 -0300

    add

--- setup/wsgi.py
+++ setup/wsgi.py
@@ -8,6 +8,6 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'setup.settings')
 try:
     application = get_wsgi_application()
 except Exception:
-    print("Erro ao iniciar WSGI:")
-    traceback.print_exc()
+    print(">>> ERRO AO INICIAR O WSGI <<<", flush=True)
+    traceback.print_exc(file=sys.stdout)
     sys.exit(1)
