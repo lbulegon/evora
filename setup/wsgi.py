63b67c4a1c7ba52f61fe2634a3369340e4a7c9b7
lbulegon <lbulegon@gmail.com>
2025-06-10 01:37:00 -0300

    add

--- setup/wsgi.py
+++ setup/wsgi.py
@@ -1,16 +1,13 @@
-"""
-WSGI config for setup project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
-"""
-
 import os
-
+import sys
+import traceback
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'setup.settings')
 
-application = get_wsgi_application()
+try:
+    application = get_wsgi_application()
+except Exception:
+    print("Erro ao iniciar WSGI:")
+    traceback.print_exc()
+    sys.exit(1)
